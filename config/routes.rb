eb96a8ce63daa9700769ce31ab9c94cff662de42
Mohammed Usama Shaheer <2019cs605@student.uet.edu.pk>
2023-06-16 15:58:26 +0000

    ddd

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   get "pages/home", to: "pages#home"
   get "pages/sign_up", to: "pages#sign_up"
   get "pages/sign_in", to: "pages#sign_in"
-  # get "pages/recipes", to: "recipes#index"
-  # get "pages/recipes/new", to: "recipes#new"
-  # post "pages/recipes", to: "recipes#create"
-  # get "pages/recipes/:id/edit", to: "recipes#edit"
-  # patch "pages/recipes/:id", to: "recipes#update"
-  # get "pages/recipes/:id", to: "recipes#show"
-  # delete "pages/recipes/:id", to: "recipes#destroy"
-  resources :recipes
+  get "/recipes", to: "recipes#index"
+  get "/recipes/new", to: "recipes#new"
+  post "/recipes", to: "recipes#create"
+  get "/recipes/:id/edit", to: "recipes#edit"
+  patch "/recipes/:id", to: "recipes#update"
+  get "/recipes/:id", to: "recipes#show"
+  delete "/recipes/:id", to: "recipes#destroy"
+  # resources :recipes
 end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -1,8 +1,18 @@
 require "test_helper"
 
 class RecipesTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef=Chef.create!(chefname: "mashrur", email: "sjadcjd@gmail.com")
+    @recipe=Recipe.create(name: "vegetable saute", description: "great vegetable sautee, add vegetable and oil", chef: @chef)
+  end
   test "should get recipes index" do
-    get recipes_url
+    get recipes_path
     assert_response :success
+  end
+  test "should get recipes listen" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_select "a[href=?]", recipes_path, text: "Recipes"
+    assert_select "a[href=?]", recipes_path, text: "New Recipe"
   end
 end
