143e659f6d129557ab2a334e59d5d2ab01000817
Mohammed Usama Shaheer <2019cs605@student.uet.edu.pk>
2023-06-16 14:47:12 +0000

    f

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   get "pages/home", to: "pages#home"
   get "pages/sign_up", to: "pages#sign_up"
   get "pages/sign_in", to: "pages#sign_in"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # resources : "pages"
+  # get "pages/recipes", to: "recipes#index"
+  # get "pages/recipes/new", to: "recipes#new"
+  # post "pages/recipes", to: "recipes#create"
+  # get "pages/recipes/:id/edit", to: "recipes#edit"
+  # patch "pages/recipes/:id", to: "recipes#update"
+  # get "pages/recipes/:id", to: "recipes#show"
+  # delete "pages/recipes/:id", to: "recipes#destroy"
+  resources :recipes
 end
